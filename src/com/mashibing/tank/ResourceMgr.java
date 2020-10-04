cf31665a53d309133778c3a64dae4e0e8d6f9921
baowuji2019 <742850510@qq.com>
2020-10-04 16:49:29 +0800

    修改了子弹的图片

--- src/com/mashibing/tank/ResourceMgr.java
+++ src/com/mashibing/tank/ResourceMgr.java
@@ -7,12 +7,20 @@ import javax.imageio.ImageIO;
 
 public class ResourceMgr {
 	public  static BufferedImage TankL,TankU,TankR,TankD;
+	public  static BufferedImage bulletL,bulletU,bulletR,bulletD;
 	static {
 		try {
 			TankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
 			TankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
 			TankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
 			TankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+			
+			bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+			bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
+			bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+			bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
