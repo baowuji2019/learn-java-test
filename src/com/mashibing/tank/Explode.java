15ce0a0270ac4b719bacd08ece1fd1ba05bebff7
baowuji2019 <742850510@qq.com>
2020-10-05 20:19:10 +0800

    添加了爆炸的声音和图片组

---
+++ src/com/mashibing/tank/Explode.java
@@ -0,0 +1,42 @@
+package com.mashibing.tank;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Explode {
+	
+	public static int WIDTH =ResourceMgr.explodes[0].getWidth();
+	public static int HEIGHT = ResourceMgr.explodes[0].getHeight();
+	
+	private int x,y;
+	private boolean living = true;
+	TankFrame tf = null;
+	private int step = 0 ;
+
+
+	public Explode(int x, int y,TankFrame tf) {
+		super();
+		this.x = x;
+		this.y = y;
+
+		this.tf = tf;
+		
+		new Audio("audio/explode.wav").play();
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(ResourceMgr.explodes[step++], x, y, null);
+		if (step >= ResourceMgr.explodes.length)
+			step = 0;
+
+		
+			
+		}
+		
+		
+
+
+	
+
+}
